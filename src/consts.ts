ac61b24b887b2890ad4e94ea1628cfc51cbb9de6
patrick.oneill <patrick.oneill@travelperk.com>
2024-01-03 18:24:49 +0000

    Add prettier, tailwind typography.
    Extracted main layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'patrickoneill.dev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "patrickoneill.dev";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const TWITTER_URL = "https://twitter.com/poneill_dev";
+export const GITHUB_URL = "https://github.com/paddyjoneill";
